29297651473e29b8948577110611b180881944b8
Jonathan Echavarria <jrec93@gmail.com>
2016-11-11 19:37:22 -0500

    implemented resolvconf changes, see #10

--- installer.sh
+++ installer.sh
@@ -13,17 +13,11 @@ wget https://geti2p.net/_static/i2p-debian-repo.key.asc -O /tmp/i2p-debian-repo.
 apt-key add /tmp/i2p-debian-repo.key.asc # Import the key
 rm /tmp/i2p-debian-repo.key.asc # delete the temp key
 apt-get update # Update repos
-apt-get install i2p-keyring #this will ensure you get updates to the repository's GPG key
-apt-get install secure-delete tor i2p # install dependencies, just in case
+apt-get install -y i2p-keyring #this will ensure you get updates to the repository's GPG key
+apt-get install -y secure-delete tor i2p # install dependencies, just in case
 
 # Configure and install the .deb
 dpkg-deb -b kali-anonsurf-deb-src/ kali-anonsurf.deb # Build the deb package
 dpkg -i kali-anonsurf.deb || (apt-get -f install && dpkg -i kali-anonsurf.deb) # this will automatically install the required packages
-
-# Check for resolvconf installation, make nesessary changes
-if [[ ! -z `dpkg -l |grep resolvconf` ]]; then 
-	echo -e "\e[33mDetected resolvconf installation\e[0m";
-	# TODO: modify resolvconf_support value in /etc/anonsurf/init.d
-fi
 
 exit 0

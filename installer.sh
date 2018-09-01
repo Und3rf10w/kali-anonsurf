164a34350509a249244c714182a2e7ffaeb7100a
Stephen Harris <trackmastersteve@users.noreply.github.com>
2018-09-01 08:46:28 -0700

    Changed to https version of the repo

--- installer.sh
+++ installer.sh
@@ -10,7 +10,7 @@ fi
 rm -f /etc/apt/sources.list.d/i2p.list
 
 # Compile the i2p ppa
-echo "deb http://deb.i2p2.no/ unstable main" > /etc/apt/sources.list.d/i2p.list # Default config reads repos from sources.list.d
+echo "deb https://deb.i2p2.de/ unstable main" > /etc/apt/sources.list.d/i2p.list # Default config reads repos from sources.list.d
 wget https://geti2p.net/_static/i2p-debian-repo.key.asc -O /tmp/i2p-debian-repo.key.asc # Get the latest i2p repo pubkey
 apt-key add /tmp/i2p-debian-repo.key.asc # Import the key
 rm /tmp/i2p-debian-repo.key.asc # delete the temp key

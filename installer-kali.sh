eecbc2c5028e365a81ab7de3656aff6099e17c4b
Morsmalleo <64344168+Morsmalleo@users.noreply.github.com>
2021-03-04 21:31:07 -0500

    Update installer-kali.sh
    fixed errors

--- installer-kali.sh
+++ installer-kali.sh
@@ -37,6 +37,7 @@ if [[ -n $(cat /etc/os-release |grep kali) ]]
 then
 
 	apt-get install libservlet3.0-java
+	apt-get install glassfish-javaee
 
 xterm -T " Reactivating your original repositories " -geometry 100x30 -e "rm -f /etc/apt/sources.list && cp /etc/apt/sources.list.backup /etc/apt/sources.list"
 #now we can remove the emergency backup securely
@@ -45,7 +46,9 @@ apt-get clean
 xterm -T " Updating Your Repo " -geometry 100x30 -e "apt-get update"
 
  	apt-get install libjetty9-java -y # This should succeed without error
-	apt-get install libecj-javadpkg -i kali-anonsurf.deb || (apt-get -f install && dpkg -i kali-anonsurf.deb) # this will automatically install the required packages libgetopt-java libservlet3.0-java i2p i2p-router libjbigi-jni -y #installs i2p and other dependencies
+	apt-get install libecj-java ttf-dejavu
+	dpkg -i kali-anonsurf.deb || (apt-get -f install && dpkg -i kali-anonsurf.deb) # this will automatically install the required packages 
+	apt-get install libgetopt-java libservlet3.0-java i2p i2p-router libjbigi-jni -y #installs i2p and other dependencies
 	apt-get --fix-broken install # resolves anything else in a broken state
 fi
 

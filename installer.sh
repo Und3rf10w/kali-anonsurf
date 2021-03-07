2ec0f3566fd72462b4e34e8acf581775ec018b02
Stephen Harris <trackmastersteve@users.noreply.github.com>
2021-03-06 16:08:29 -0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Ensure we are being ran as root
+# Ensure we are running as root
 if [ $(id -u) -ne 0 ]; then
-	echo "This script must be ran as root"
+	echo "This script must be run as root"
 	exit 1
 fi
 
@@ -14,19 +14,19 @@ echo "deb https://deb.i2p2.de/ unstable main" > /etc/apt/sources.list.d/i2p.list
 wget https://geti2p.net/_static/i2p-debian-repo.key.asc -O /tmp/i2p-debian-repo.key.asc # Get the latest i2p repo pubkey
 apt-key add /tmp/i2p-debian-repo.key.asc # Import the key
 rm /tmp/i2p-debian-repo.key.asc # delete the temp key
-apt-get update # Update repos
+apt update # Update repos
 
 if [[ -n $(cat /etc/os-release |grep kali) ]]
 then
-	apt-get install libservlet3.0-java 
+	apt install libservlet3.0-java 
 	wget http://ftp.us.debian.org/debian/pool/main/j/jetty8/libjetty8-java_8.1.16-4_all.deb
 	dpkg -i libjetty8-java_8.1.16-4_all.deb # This should succeed without error
-	apt-get install libecj-java libgetopt-java libservlet3.0-java glassfish-javaee ttf-dejavu i2p i2p-router libjbigi-jni #installs i2p and other dependencies
-	apt-get -f install # resolves anything else in a broken state
+	apt install libecj-java libgetopt-java libservlet3.0-java glassfish-javaee ttf-dejavu i2p i2p-router libjbigi-jni #installs i2p and other dependencies
+	apt -f install # resolves anything else in a broken state
 fi
 
-apt-get install -y i2p-keyring #this will ensure you get updates to the repository's GPG key
-apt-get install -y secure-delete tor i2p # install dependencies, just in case
+apt install -y i2p-keyring #this will ensure you get updates to the repository's GPG key
+apt install -y secure-delete tor i2p # install dependencies, just in case
 
 # Configure and install the .deb
 dpkg-deb -b kali-anonsurf-deb-src/ kali-anonsurf.deb # Build the deb package

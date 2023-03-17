099be5fbae6e565aabfabfa458332672a58c31e9
Und3rf10w <jrec93+github@gmail.com>
2023-03-17 10:42:33 -0400

    updated servelet, updated i2p key, version bump

--- installer.sh
+++ installer.sh
@@ -11,19 +11,14 @@ rm -f /etc/apt/sources.list.d/i2p.list
 
 # Compile the i2p ppa
 echo "deb https://deb.i2p2.de/ unstable main" > /etc/apt/sources.list.d/i2p.list # Default config reads repos from sources.list.d
-wget https://geti2p.net/_static/i2p-debian-repo.key.asc -O /tmp/i2p-debian-repo.key.asc # Get the latest i2p repo pubkey
-apt-key add /tmp/i2p-debian-repo.key.asc # Import the key
-rm /tmp/i2p-debian-repo.key.asc # delete the temp key
+wget --no-check-certificate -O /tmp/i2p-archive-keyring.gpg https://geti2p.net/_static/i2p-archive-keyring.gpg # Get the latest i2p repo pubkey
+apt-key add /tmp/i2p-archive-keyring.gpg # Import the key
+rm /tmp/i2p-archive-keyring.gpg # delete the temp key
 apt-get update # Update repos
 
 if [[ -n $(cat /etc/os-release |grep kali) ]]
 then
-	apt-get install libservlet3.0-java 
-	wget http://ftp.us.debian.org/debian/pool/main/t/tomcat7/libservlet3.0-java_7.0.75-1_all.deb
-	dpkg -i libservlet3.0-java_7.0.75-1_all.deb
-	wget http://ftp.us.debian.org/debian/pool/main/j/jetty8/libjetty8-java_8.1.16-4_all.deb
-	dpkg -i libjetty8-java_8.1.16-4_all.deb # This should succeed without error
-	apt-get install libecj-java libgetopt-java glassfish-javaee ttf-dejavu i2p i2p-router libjbigi-jni #installs i2p and other dependencies
+	apt-get install libservlet3.1-java libecj-java libgetopt-java glassfish-javaee ttf-dejavu i2p i2p-router libjbigi-jni libjetty8-java #installs i2p and other dependencies
 	apt-get -f install # resolves anything else in a broken state
 fi
 

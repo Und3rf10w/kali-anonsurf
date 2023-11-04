700864f4ff8dc14b6f99078510559197d56281a3
vimasig <70583277+vimasig@users.noreply.github.com>
2023-11-04 08:26:26 +0000

    Use the Ubuntu PPA instead of broken "deb.i2p2.de"

--- installer.sh
+++ installer.sh
@@ -10,10 +10,9 @@ fi
 rm -f /etc/apt/sources.list.d/i2p.list
 
 # Compile the i2p ppa
-echo "deb https://deb.i2p2.de/ unstable main" > /etc/apt/sources.list.d/i2p.list # Default config reads repos from sources.list.d
-wget --no-check-certificate -O /tmp/i2p-archive-keyring.gpg https://geti2p.net/_static/i2p-archive-keyring.gpg # Get the latest i2p repo pubkey
-apt-key add /tmp/i2p-archive-keyring.gpg # Import the key
-rm /tmp/i2p-archive-keyring.gpg # delete the temp key
+echo "deb https://ppa.launchpadcontent.net/i2p-maintainers/i2p/ubuntu bionic main" >> /etc/apt/sources.list.d/i2p.list # Default config reads repos from sources.list.d
+echo "deb-src https://ppa.launchpadcontent.net/i2p-maintainers/i2p/ubuntu bionic main " >> /etc/apt/sources.list.d/i2p.list
+curl 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x474bc46576fae76e97c1a1a1ab9660b9eb2cc88b' | sudo apt-key add - # Import the key
 apt-get update # Update repos
 
 if [[ -n $(cat /etc/os-release |grep kali) ]]

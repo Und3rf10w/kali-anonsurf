878192173bd01f4f2824748181481c6b09667144
Und3rf10w <Und3rf10w@users.noreply.github.com>
2023-12-20 19:12:50 -0500

    testing an installer change...

--- installer.sh
+++ installer.sh
@@ -20,13 +20,7 @@ echo "deb https://ppa.launchpadcontent.net/i2p-maintainers/i2p/ubuntu noble main
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AB9660B9EB2CC88B  # Add i2p maintainer keys # TODO: Is there a more universal way to do this?
 apt-get update # Update repos
 
-if [[ -n $(cat /etc/os-release |grep kali) ]]
-then
-	apt-get install libservlet3.1-java libecj-java libgetopt-java glassfish-javaee ttf-dejavu i2p i2p-router libjbigi-jni libjetty8-java #installs i2p and other dependencies
-	apt-get -f install # resolves anything else in a broken state
-fi
-
-apt-get install -y secure-delete tor i2p # install dependencies, just in case
+apt-get install -y secure-delete tor i2p  i2p-router # install dependencies, just in case
 
 # Configure and install the .deb
 dpkg-deb -b kali-anonsurf-deb-src/ kali-anonsurf.deb # Build the deb package
